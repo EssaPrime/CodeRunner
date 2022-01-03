6c11e347c8f42baab9d890ac7b727569fcd8333e
EssaPrime <76210736+EssaPrime@users.noreply.github.com>
2022-01-03 10:32:25 +0400

    Initial commit

---
+++ CodeRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using MoonSharp.Interpreter;
+using System;
+namespace CodeRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lua Code Runner By ! EssaPrime#0001";
+            Script script = new Script();
+            Console.WriteLine(script.DoFile(args[0]));
+            Console.ReadKey();
+        }
+    }
+}
